f5f3db7fa2fe306d470ee388f583adebba7fae88
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-09-01 18:51:21 +0600

    Java Interface

---
+++ Interface_example.java
@@ -0,0 +1,68 @@
+interface bank {
+    float interestRate();
+}
+
+class SBI implements bank {
+    public float interestRate() {
+        return 8.5f;
+    }
+}
+
+class DBBL implements bank {
+    public float interestRate() {
+        return 9.5f;
+    }
+}
+
+/* multiple inheritance is not supported in the case of
+class because of ambiguity. However, it is supported
+in case of an interface because there is no ambiguity.
+It is because its implementation is provided by the
+implementation class. */
+
+interface Printable {
+    void print();
+}
+
+interface Readable {
+    void print();
+}
+
+class Printer implements Printable, Readable {
+    public void print() {
+        System.out.println("Printing...");
+    }
+}
+
+/* Interface inheritance */
+
+interface Showable extends Printable {
+    void show();
+}
+
+class Printer2 implements Showable {
+    public void print() {
+        System.out.println("Inheritance Printing...");
+    }
+    public void show() {
+        System.out.println("Inheritance Showing...");
+    }
+}
+
+public class Interface_example {
+    public static void main(String[] args) {
+        bank b = new SBI();
+        System.out.println("Interest rate on SBI is " + b.interestRate());
+        b = new DBBL();
+        System.out.println("Interest rate on DBBL is " + b.interestRate());
+        Printer p = new Printer();
+        p.print();
+
+        // Showable is a subtype of Printable and Readable
+        // so we can use it as a Printable and Readable
+        // Showable p2 = new Printer2();
+        Printer2 p2 = new Printer2();
+        p2.print();
+        p2.show();
+    }
+}
